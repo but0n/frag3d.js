ab126ce842711a284bfb32da8fb2f4ac4c5627ac
but0n <but0n@icloud.com>
2018-06-18 12:20:48 +0800

    feat: debug animation with wheel, enhance shader

--- static/scripts/frag3d.js
+++ static/scripts/frag3d.js
@@ -45,28 +45,42 @@ shader.a_UV = [mesh.texCoords, 2, fr.gl.FLOAT];
 let sampler = [];
 let size = 1024;
 for(let i = 0; i < size**2; i++) {
-    sampler.push(255*Math.random(), 255*Math.random(), 255*Math.random());
+    sampler.push(255*Math.random(), 255*Math.random(), 255*Math.random(), 255*Math.random());
 }
-fr.genTexture(0, 0, fr.gl.RGB, size, size, 0, fr.gl.UNSIGNED_BYTE, new Uint8Array(sampler));
-shader.u_tex0 = 0;
+fr.genTexture(0, 0, fr.gl.RGBA, size, size, 0, fr.gl.UNSIGNED_BYTE, new Uint8Array(sampler));
+shader.u_noise = 0;
 
 fr.bindBuffer(mesh.map, fr.gl.STATIC_DRAW);
 fr.gl.drawElements(fr.gl.TRIANGLES, mesh.map.length, fr.gl.UNSIGNED_SHORT, 0);
-let a = 0.001;
-setTimeout(() => {
-    let proce = setInterval(() => {
-        t+=a;
-        // else if(t>0)
-        //     t = t - 1;
-        // if(t <= 2)
-        // if(Math.round(t) == 18)
-        if(a < 0.26) {
-            a += 0.00002;
-        }
-        shader.u_time = t;
-        fr.gl.drawElements(fr.gl.TRIANGLES, mesh.map.length, fr.gl.UNSIGNED_SHORT, 0);
-            // clearInterval(proce);
-        // }
-    }, 1)
-}, 1000)
+let a = 0;
+// setTimeout(() => {
+//     let proce = setInterval(() => {
+//         t+=a;
+//         // else if(t>0)
+//         //     t = t - 1;
+//         // if(t <= 2)
+//         // if(Math.round(t) == 18)
+//         if(a < 0.26) {
+//             a += 0.00002;
+//         }
+//         shader.u_time = t;
+//         fr.gl.drawElements(fr.gl.TRIANGLES, mesh.map.length, fr.gl.UNSIGNED_SHORT, 0);
+//             // clearInterval(proce);
+//         // }
+//     }, 1)
+// }, 1000)
 // fr.ss_render();
+
+$('#renderer').bind('wheel', function(e){
+    const delta = Math.round(e.originalEvent.deltaY);
+    console.log(delta);
+    a += delta;
+    shader.u_time = a;
+    fr.gl.drawElements(fr.gl.TRIANGLES, mesh.map.length, fr.gl.UNSIGNED_SHORT, 0);
+
+    // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_COLOR_BIT);
+    // deepth += delta*.1;
+    // view.setLookAt(0, 0, deepth, 0, 0, 0, 0, 1, 0);
+    // gl.uniformMatrix4fv(viewLocation, false, view.elements);
+    // gl.drawElements(gl.TRIANGLES, mod.map.length, gl.UNSIGNED_SHORT, 0); // Render
+});
